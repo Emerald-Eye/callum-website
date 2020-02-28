3604a70533a319ea7e0bd4e6d9e3afa113956d45
Callum <ccallum.wong@gmail.com>
2020-02-28 15:22:14 +1100

    master

--- leave/script.js
+++
@@ -1,7 +0,0 @@
-function redirect() {
-  if (document.querySelector('input[name="location"]:checked').value == "cw.com") {
-    window.location.href = "http://callumwong.com";
-  } else if (document.querySelector('input[name="location"]:checked').value == "github") {
-    window.location.href = "https://emerald-eye.github.io/callums-website";
-  }
-}
